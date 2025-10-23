569a7f3734c3fe824e035dec436100672a2eb65b
Prajwal-H-Rao <prajwalhr130@gmail.com>
2025-10-23 17:17:54 +0530

    changed jwt.ts to use prisma model

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -4,4 +4,9 @@ const prisma = new PrismaClient({
   log: ["query", "info", "warn", "error"],
 });
 
+process.on("SIGINT", async () => {
+  await prisma.$disconnect();
+  process.exit(0);
+});
+
 export default prisma;
